99bef1e2f56d5d692ded9eb2239852520f48fa76
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-09-24 12:10:58 -0500

    feat: add shared type definition files

---
+++ packages/shared-types/src/translate.d.ts
@@ -0,0 +1,15 @@
+export type TranslationRequest = {
+    sourceLanguageCode: string;
+    targetLanguageCode: string;
+    sourceText: string;
+}
+
+export type TranslationResponse = {
+    timestamp: string;
+    targetText: string;
+}
+
+export type TranslationDBObject = TranslationRequest & TranslationResponse & {
+    requestId: string;
+}
+
