c889558feaa45ab867638f02cd8225b0bc774019
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-10-29 14:54:47 -0500

    fix: update banner text

--- apps/frontend/src/components/banner.tsx
+++ apps/frontend/src/components/banner.tsx
@@ -1,7 +1,7 @@
 export const Banner = () => {
     return (
         <div className="bg-black text-white p-2 text-sm text-center">
-            <p>Verbo is currently in development mode. Read updates on <a href="https://sergiopichardo.com/projects/verbo" target="_blank" className="text-blue-500">Verbo Translator</a>.</p>
+            <p>Verbo is currently under development. Check updates on <a href="https://sergiopichardo.com/projects/verbo" target="_blank" className="text-blue-500">Verbo Translator</a>.</p>
         </div>
     )
 }
